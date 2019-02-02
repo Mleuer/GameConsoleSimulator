e17f7c563736cc43c73a674a6a3d6e0fa6276db0
Adam J. Leuer <aleuer2@illinois.edu>
2019-02-02 17:40:28 -0600

    Made Games comparable

--- GameConsoleSimulator/Models/Game.cs
+++ GameConsoleSimulator/Models/Game.cs
@@ -2,8 +2,25 @@ using System;
 
 namespace GameConsoleSimulator.Models
 {
-    public class Game
+    public class Game : IComparable, IComparable<Game>
     {
         public String Title { get; set; }
+        
+        public int CompareTo(object obj)
+        {
+            if (obj.GetType() == typeof(Game))
+            {
+                return CompareTo(other: (Game) obj);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public int CompareTo(Game other)
+        {
+            return String.Compare(this.Title, other.Title, StringComparison.Ordinal);
+        }
     }
 }
